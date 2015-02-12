93f7b7ecd4bdc720d1de52a683901c37a69d8cac
spirinvladimir <spirin.vladimir@gmail.com>
2015-02-13 01:52:32 +0700

    pause & fix max stack

--- index.js
+++ index.js
@@ -17,21 +17,26 @@ domready(function () {
             }
         }),
         replay = function () {
-            snake.kill();
-            snake = new Snake({
-                ex: ex,
-                game: game,
-                color: {
-                    head: ex.Color.Red,
-                    body: ex.Color.Yellow,
-                    food: ex.Color.Green
-                }
+            game.rootScene.children.forEach(function (actor) {
+                actor.kill();
             });
-            snake.onLose = replay;
-            snake.run({x: 1});
-        };
-    window.onkeydown = function (k) {
-        k = k.keyCode;
+            setTimeout(function () {
+                snake = new Snake({
+                    ex: ex,
+                    game: game,
+                    color: {
+                        head: ex.Color.Red,
+                        body: ex.Color.Yellow,
+                        food: ex.Color.Green
+                    }
+                });
+                snake.onLose = replay;
+                snake.run({x: 1});
+            }, 500);
+        },
+        paused = false;
+    game.input.keyboard.on('down', function (k) {
+        k = k.key;
         if (k === 37) {
             snake.left();
         } else if (k === 38) {
@@ -40,8 +45,16 @@ domready(function () {
             snake.right();
         } else if (k === 40) {
             snake.down();
+        } else if (k === 32) {
+            if (paused) {
+                game.start();
+                paused = false;
+            } else {
+                game.stop();
+                paused = true;
+            }
         }
-    };
+    });
     document.addEventListener('click', function () {
         replay();
     });
